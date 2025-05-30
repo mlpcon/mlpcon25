e501d76cc61129334a7c94b62476316a652887c0
Corpulent Brony <corpulent@brony.email>
2025-05-30 09:47:11 -0400

    Update consts.ts
    Added first couple items on schedule to test

--- src/consts.ts
+++ src/consts.ts
@@ -1,19 +1,33 @@
 import { withBase } from './utils';
 
 export const SCHEDULE: {
-  title: string;
-  description: string;
-  datetime: number; // UNIX
-  duration: number; // MINUTES
-  channel: 1 | 2;
+	title: string;
+	description: string;
+	datetime: number; // UNIX
+	duration: number; // MINUTES
+	channel: 1 | 2;
 }[] = [
-  // {
-  //   title: 'title',
-  //   description: 'description',
-  //   duration: 30,
-  //   datetime: 1741859253893,
-  //   channel: 1,
-  // },
+	{
+		title: "Opening Maremonies", 
+		description: "TBD", 
+		duration: 45, 
+		datetime: Date.parse("2025-07-04T11:00Z"), 
+		channel: 1
+	}, 
+	{
+		title: "Amending Fences 10th Anniversary", 
+		description: "It's been ten years since Twilight went back to Canterlot to convince her old friend Moondancer to give friendship another chance!  Join us as we rewatch the best episode of Season 5 and its related fan content!", 
+		duration: 45, 
+		datetime: Date.parse("2025-07-04T12:00Z"), 
+		channel: 1
+	}
+	// {
+	//   title: 'title',
+	//   description: 'description',
+	//   duration: 30,
+	//   datetime: 1741859253893,
+	//   channel: 1,
+	// },
 ].sort((a, b) => a.datetime - b.datetime);
 
 /**
@@ -23,16 +37,16 @@ export const SCHEDULE: {
  * @param format - image format (jpg, png, etc.)
  */
 const getImages = (count: number, folder: string, format: string) => {
-  return new Array(count)
-    .fill(0)
-    .map((_, i) => withBase(`/images/vendors/${folder}/${i + 1}.${format}`));
+	return new Array(count)
+		.fill(0)
+		.map((_, i) => withBase(`/images/vendors/${folder}/${i + 1}.${format}`));
 };
 
 export const VENDORS = [
-  // {
-  //   title: 'title',
-  //   description: 'description.',
-  //   link: 'link',
-  //   images: getImages(5, 'folder', 'format'),
-  // },
+	// {
+	//   title: 'title',
+	//   description: 'description.',
+	//   link: 'link',
+	//   images: getImages(5, 'folder', 'format'),
+	// },
 ];
