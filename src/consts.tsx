137dfc0f9987669d4f0e5cce7c3103c44651650a
Corpulent Brony <corpulent@brony.email>
2025-06-10 15:21:08 -0400

    Changed duration of Gamejam showcase: Your choice edition!
    Also had to push back start times of Live CYOA III: Anonfilly Goes to Manehatten and RarityFest to correspond

--- src/consts.tsx
+++ src/consts.tsx
@@ -215,7 +215,7 @@ export const SCHEDULE: {
 			Welcome to the fifth /mlp/ game jam! We'll once again be taking a look st the submitted games from our VERY talented game devs in the board.<br />
 			This time around I'll be letting (You) chose the theme for this little jam! That's right! We'll be doing a theme chosen by the board! So go on ahead and see if it's not too late to make your voice heard! And if the theme is selected, then feel free to participate in the jam! Check the catalog by typing "game jam" in the search bar to find both threads!
 		</>,
-		duration: 120,
+		duration: 180,
 		datetime: Date.parse("2025-07-06T22:00Z"),
 		channel: 1
 	},
